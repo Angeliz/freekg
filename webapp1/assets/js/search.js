6e0188ad85a974ada2026b7e9c5b963fa4bd32a7
Angeliz <471330418@qq.com>
2018-01-15 15:49:09 +0800

    add

--- webapp1/assets/js/search.js
+++ webapp1/assets/js/search.js
@@ -1,22 +1,22 @@
+/**
+ * Created by 67 on 2017/5/26.
+ * BUG: 不能查询同名内容
+ */
+/**
+ * 用于首页查询功能
+ */
 var pareparam;
 var count=0;//消歧计数器
 var pausejsonlist;
 var uri=0;
-// var dom = document.getElementById("search_shower");
+
 function search() {
-    // $("#search_shower").hide();
-    // deleteambiguity();
     var info = document.getElementById("Search-box").value;
-    // if (info == "") {
-    //     info = "除却巫山不是云";
-    // }
-    // var realinfo = convert(1, info);
     $.getJSON("http://www.freekg.cn/poet/datasource/unknownquery?name=" + info, function (json) {
         console.log("新方法查询结果说明");
         console.log(json);
         if (json.length > 1) {//如果不止一个元素就消歧
             pausejsonlist = [];
-            // $("#search_shower").hide();
             ambiguity(json);
             var len = json.length;
             for (let a = 0; a < len; a++) {
@@ -30,20 +30,17 @@ function search() {
             if (json[0].born) {//判断单个实体的类型，是古诗还是诗人
                 uri=json[0].id;
                 answerpoetrequest(info);
-
-
-                // console.log(uri);
-                // alert(uri);
             } else {
                 answerpoemrequest(info);
             }
-            // $("#search_shower").show();
         }
     });
 }
+// 响应诗人查询结果，页面跳转
 function answerpoetrequest(info) {
     window.location.href="http://www.freekg.cn/poet/poetinfo.html?poetname="+info+"?uri=http://www.freekg.com/poet/"+uri;
 }
+// 响应古诗题目或古诗内容查询结果，页面跳转
 function answerpoemrequest(info) {
     window.location.href="http://www.freekg.cn/poet/poeminfo.html?poemname="+info;
 }
