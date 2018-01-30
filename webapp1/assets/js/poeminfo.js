fed06a6d35558d938ef8f070e88241863b12c4cd
Angeliz <471330418@qq.com>
2018-01-30 22:47:05 +0800

    诗歌主页

--- webapp1/assets/js/poeminfo.js
+++ webapp1/assets/js/poeminfo.js
@@ -1,13 +1,23 @@
 /**
- * Created by Felixy on 2017/11/25.
+ * Created by 67 on 2018/1/30.
+ * 没有消歧处理
  */
-<!--设置head位置显示的诗人姓名标签-->
 var thisURL = document.URL;
 var  getval =thisURL.split('?')[1];
 var poem= decodeURIComponent(getval.split("=")[1]);//将url字符解码为utf8编码后的诗歌姓名
 console.log(poem);
-$(function () {
-    $.getJSON("datasource/poemquery?name="+convert(1,poem),function (json) {
-        console.log(convert(0,json));
-    });
-})
+var content=document.getElementById('poem_content');
+
+$.getJSON("http://www.freekg.cn/poet/datasource/unknownquery?name="+poem,function (json) {
+    console.log(json);
+    console.log(1);
+    var poemname=document.createElement("h4");
+    var poemauthor=document.createElement("p");
+    var poemcontent=document.createElement("h5");
+    poemname.innerText=json[0].name;
+    poemauthor.innerText=json[0].author;
+    poemcontent.innerText=json[0].content;
+    content.appendChild(poemname);
+    content.appendChild(poemauthor);
+    content.appendChild(poemcontent);
+});
